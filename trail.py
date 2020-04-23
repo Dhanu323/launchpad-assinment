b1ed608a6b521140c1527dde334b3a9b60c17f10
Dhanu323 <63581712+Dhanu323@users.noreply.github.com>
2020-04-23 22:15:23 +0530

    Add files via upload
    Reverse Dictionary Fetch

---
+++ trail.py
@@ -0,0 +1,7 @@
+val = input("enter the value")
+dict = {"hello" : "world", "speckbit" : "self-learning", "life" : "meaning"}
+for key, value in dict.items():
+    if val == value:
+        box = key
+        print(box)
+           
